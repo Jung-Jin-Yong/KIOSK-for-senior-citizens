2786533080f48fd735fb85e4c16b169f87ae349d
Jung Jin Yong <61494096+Jung-Jin-Yong@users.noreply.github.com>
2020-08-03 21:58:48 +0900

    Version 0.5

--- engine.js
+++ engine.js
@@ -1,1 +1,17 @@
-const onlyHamburger=
+window.onload = function(){
+  var menuButton1 = document.querySelectorAll(".menuButton1");
+  var menuButton2 = document.querySelectorAll('.menu2');
+
+  menuButton1[0].addEventListener('click',()=>{
+    console.log('확인');
+    localStorage.setItem(`firstStep`,`0`);
+  });
+  menuButton1[1].addEventListener('click',()=>{
+    console.log('확인');
+    localStorage.setItem(`firstStep`,`1`);
+  });
+  menuButton1[2].addEventListener('click',()=>{
+    console.log('확인');
+    localStorage.setItem(`firstStep`,`2`);
+  });
+}
