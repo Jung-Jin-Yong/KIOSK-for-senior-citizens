dc56adcae22d4bc807b1520b513bc9eace6eb594
Jung Jin Yong <61494096+Jung-Jin-Yong@users.noreply.github.com>
2020-08-29 17:33:41 +0900

    Version 0.9.1

--- engine4.js
+++ engine4.js
@@ -1,8 +1,7 @@
 window.onload = function(){
-  const menuObject = [['bigMcSet','0','beef','tomato','빅맥 세트'],['doubleBeefBurgerSet','0','beef','tomato','더블 불고기버거세트'],['doubleCheezeBurgerSet','0','beef','cheeze','더블 치즈버거세트'],['eggBeefBurgerSet','0','beef','egg','에그불고기버거 세트'],['shshBurger','0','shrimp','tomato','슈슈 버거'],['beefBurgerSet','1','beef','tomato', '불고기 버거 세트'],['cheezeBurger','1','pork','cheeze','치즈버거 단품'],['burger1955Set','1','beef','cheeze','tomato', '1955버거 세트'],['MacChickenSet','1','chicken','tomato', '맥치킨 세트'],['oreoMcFlurry','2', '오레오 맥플러리'],['bananaOreoMcFlurry','2','바나나오레오 맥플러리'],['strawBerryOreoMcFlurry','2', '딸기 오레오 멕플러리'],['iceCreamCone','2', '아이스크림콘'],['ChocoCone','2', '초코콘'],['caramelSundayIceCream','2', '카라멜 선데이 아이스크림'],['applePie','2', '애플파이']];
+  const menuObject = [['bigMcSet','0','beef','tomato','빅맥 세트'],['doubleBeefBurgerSet','0','beef','tomato','더블 불고기버거세트'],['doubleCheezeBurgerSet','0','beef','cheeze','더블 치즈버거세트'],['eggBeefBurgerSet','0','beef','egg','에그불고기버거 세트'],['shshBurger','0','shrimp','tomato','슈슈 버거'],['beefBurgerSet','1','beef','tomato', '불고기 버거 세트'],['cheezeBurger','1','pork','cheeze','치즈버거 단품'],['burger1955Set','1','pork','cheeze','tomato', '1955버거 세트'],['MacChickenSet','1','chicken','tomato', '맥치킨 세트'],['oreoMcFlurry','2', '오레오 맥플러리'],['bananaOreoMcFlurry','2','바나나오레오 맥플러리'],['strawBerryOreoMcFlurry','2', '딸기 오레오 멕플러리'],['iceCreamCone','2', '아이스크림콘'],['ChocoCone','2', '초코콘'],['caramelSundayIceCream','2', '카라멜 선데이 아이스크림'],['applePie','2', '애플파이']];
 
   const wantingList = localStorage.wantingList.split(',');
-  const hatingList = localStorage.hatingList.split(',');
   const firstStep = localStorage.firstStep;
   const recommendMenuImage = document.getElementById('recommendMenuImage');
   const showMenuName = document.querySelector('#recommendMenu');
@@ -20,7 +19,7 @@ window.onload = function(){
           }
         }
         if(goodpoint>0){
-          randomMenuList.splice(0,0,menuObject[j][4]);
+          randomMenuList.splice(0,0,menuObject[j][menuObject[j].length-1]);
           randomMenuListForSearchingImage.splice(0,0,menuObject[j][0]);
           goodpoint = 0;
         }
