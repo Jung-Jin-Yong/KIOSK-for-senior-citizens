e6a266a5bf64a18f728441d44d1509cdd0d97fd6
Jung Jin Yong <61494096+Jung-Jin-Yong@users.noreply.github.com>
2020-09-15 15:50:46 +0900

    Version 0.14.1

--- engine4.js
+++ engine4.js
@@ -1,6 +1,6 @@
 window.onload = function(){
   const menuObject = [['McChickenMozzarellaSet', '0', 'chicken','cheeze','6900원','맥치킨 모짜렐라 세트'],['bigMcSet','0','beef','tomato','7000원','빅맥 세트'],['doubleBeefBurgerSet','0','beef','tomato','6000원','더블 불고기버거세트'],['doubleCheezeBurgerSet','0','pork','cheeze','6500원','더블 치즈버거세트'],['eggBeefBurgerSet','0','beef','egg','5900원','에그불고기버거 세트'],['shshBurger','0','shrimp','tomato','5500원','슈슈 버거'],['beefBurgerSet','1','beef','tomato','4500원','불고기 버거 세트'],['cheezeBurger','1','pork','cheeze','4000원','치즈버거 단품'],['burger1955Set','1','pork','cheeze','tomato','4900원','1955버거 세트'],['MacChickenSet','1','chicken','tomato','4900원','맥치킨 세트'],['oreoMcFlurry','2','2500원','오레오 맥플러리'],['bananaOreoMcFlurry','2','2500원','바나나오레오 맥플러리'],['strawBerryOreoMcFlurry','2','2900원','딸기 오레오 멕플러리'],['iceCreamCone','2','1500원','아이스크림콘'],['ChocoCone','2','1500원','초코콘'],['caramelSundayIceCream','2','1500원','카라멜 선데이 아이스크림'],['applePie','2','2000원','애플파이']];
-  //메뉴 객체 파트입니다. menuObject[0~8]까지는 버거포함 메뉴, menuObject[9]부터는 디저트 메뉴입니다.
+  //메뉴 객체 파트입니다. menuObject[0~9]까지는 버거포함 메뉴, menuObject[10]부터는 디저트 메뉴입니다.
 
   const wantingList = localStorage.wantingList.split(',');
   const firstStep = localStorage.firstStep;
